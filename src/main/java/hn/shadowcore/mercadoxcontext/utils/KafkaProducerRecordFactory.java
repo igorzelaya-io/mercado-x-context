d80e428638cd1aad22c78a95f0d85f84b73c1d75
Igor A. Zelaya <izelaya@baas.hn>
2025-07-25 03:28:37 -0600

    develop: Updated RecordFactory class

--- src/main/java/hn/shadowcore/mercadoxcontext/utils/KafkaProducerRecordFactory.java
+++ src/main/java/hn/shadowcore/mercadoxcontext/utils/KafkaProducerRecordFactory.java
@@ -1,15 +1,23 @@
 package hn.shadowcore.mercadoxcontext.utils;
 
-import hn.shadowcore.mercadoxlibrary.entity.response.dto.EmailEventDto;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.nio.charset.StandardCharsets;
 
-public class KafkaProducerRecordFactory<T> {
+public class KafkaProducerRecordFactory {
 
-    public ProducerRecord<String, EmailEventDto<T>> generateOrgIdHeaders(String topic, EmailEventDto<T> eventDto) {
-        ProducerRecord<String, EmailEventDto<T>> producerRecord = new ProducerRecord<>(topic, eventDto);
-        producerRecord.headers().add("x-org-id", OrgIdContextHolder.getTenantId().getBytes(StandardCharsets.UTF_8));
+    public static <T> ProducerRecord<String, T> buildWithOrgIdHeader(String topic, String key, T eventPayload) {
+        ProducerRecord<String, T> producerRecord;
+        if(key != null) {
+            producerRecord = new ProducerRecord<>(topic, key, eventPayload);
+        }
+        else {
+            producerRecord = new ProducerRecord<>(topic, eventPayload);
+        }
+        String tenantId = OrgIdContextHolder.getTenantId();
+        if(tenantId != null) {
+            producerRecord.headers().add("x-org-id", OrgIdContextHolder.getTenantId().getBytes(StandardCharsets.UTF_8));
+        }
         return producerRecord;
     }
 }
