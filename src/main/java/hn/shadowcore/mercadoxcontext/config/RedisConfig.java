027c425f79b688825fb1ac8cef3c5defc7e90d36
Igor A. Zelaya <izelaya22@gmail.com>
2026-02-20 00:20:33 -0600

    dev: Updated RedisConfig.class for CacheableManager

--- src/main/java/hn/shadowcore/mercadoxcontext/config/RedisConfig.java
+++ src/main/java/hn/shadowcore/mercadoxcontext/config/RedisConfig.java
@@ -3,10 +3,12 @@ package hn.shadowcore.mercadoxcontext.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -19,7 +21,6 @@ import java.time.Duration;
 @Configuration
 @EnableCaching
 public class RedisConfig {
-
 
     @Bean
     public RedisCacheConfiguration redisCacheConfiguration() {
@@ -40,6 +41,16 @@ public class RedisConfig {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public CacheManager cacheManager(
+            RedisConnectionFactory connectionFactory,
+            RedisCacheConfiguration redisCacheConfiguration
+    ) {
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
     }
 
     @Bean
